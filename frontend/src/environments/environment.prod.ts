128ecd06d4cc089e70d541180644bad2563f0c4f
samaelgomez <samaelgomezherrera@gmail.com>
2021-11-22 20:27:28 +0100

    Frontend and backend dockerized

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  urlProducts: 'http://localhost:5000/api/products',
+  urlCategories: 'http://localhost:5000/api/categories'
 };
